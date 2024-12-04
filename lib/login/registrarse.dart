ecd945b932de913b03edc878906cd5a5bda220da
DilanC ChN <2002dilanchoque@gmail.com>
2024-12-04 17:46:06 -0400

    login hecho

--- lib/login/registrarse.dart
+++ lib/login/registrarse.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Registrarse extends StatefulWidget {
   @override
@@ -6,16 +7,56 @@ class Registrarse extends StatefulWidget {
 }
 
 class _RegistrarseState extends State<Registrarse> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   bool _obscurePassword = true;
+
+  Future<void> _registerUser() async {
+    String name = _nameController.text.trim();
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
+
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('usuario-app')
+          .where('email', isEqualTo: email)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        // Usuario ya registrado
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('El usuario ya está registrado')),
+        );
+      } else {
+        // Registrar nuevo usuario
+        await FirebaseFirestore.instance.collection('usuario-app').add({
+          'name': name,
+          'email': email,
+          'password': password,
+        });
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Usuario registrado exitosamente')),
+        );
+
+        Navigator.pushReplacementNamed(context, '/comienzaviajar');
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFAF3E3), // Fondo beige
+      backgroundColor: const Color(0xFFFAF3E3),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
-        iconTheme: const IconThemeData(color: Color(0xFF2E3B4E)), // Icono de cerrar
+        iconTheme: const IconThemeData(color: Color(0xFF2E3B4E)),
         title: const Text(
           "Registrarse",
           style: TextStyle(
@@ -25,110 +66,107 @@ class _RegistrarseState extends State<Registrarse> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // Logo
-            Center(
-              child: Image.asset(
-                'assets/imagenes/logo.png',
-                height: 320,
-                width: 320,
-              ),
-            ),
-            const SizedBox(height: 20),
-            // Campo de texto para Nombre
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Nombre",
-                filled: true,
-                fillColor: const Color(0xFFFDFCFB),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none,
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 20,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Center(
+                child: Image.asset(
+                  'assets/imagenes/logo.png',
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.5,
                 ),
               ),
-            ),
-            const SizedBox(height: 15),
-            // Campo de texto para Correo
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Correo",
-                filled: true,
-                fillColor: const Color(0xFFFDFCFB),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none,
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 15),
-            // Campo de texto para Contraseña
-            TextField(
-              obscureText: _obscurePassword,
-              decoration: InputDecoration(
-                hintText: "Contraseña",
-                filled: true,
-                fillColor: const Color(0xFFFDFCFB),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none,
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 20,
-                ),
-                suffixIcon: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _obscurePassword = !_obscurePassword;
-                    });
-                  },
-                  child: Icon(
-                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
-                    color: const Color(0xFF2E3B4E),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  hintText: "Nombre",
+                  filled: true,
+                  fillColor: const Color(0xFFFDFCFB),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            // Botón "Continuar"
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/comienzaviajar');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF2E3B4E),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 80,
-                ),
-              ),
-              child: const Text(
-                "Continuar",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+              const SizedBox(height: 15),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  hintText: "Correo",
+                  filled: true,
+                  fillColor: const Color(0xFFFDFCFB),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 15),
+              TextField(
+                controller: _passwordController,
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  hintText: "Contraseña",
+                  filled: true,
+                  fillColor: const Color(0xFFFDFCFB),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                    child: Icon(
+                      _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                      color: const Color(0xFF2E3B4E),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: _registerUser,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF2E3B4E),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 80,
+                  ),
+                ),
+                child: const Text(
+                  "Registrarse",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
